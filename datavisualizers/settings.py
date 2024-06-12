5806b02cec00bfee97425db4bcd35935325bb407
Keith Jung <60eokk@gmail.com>
2024-06-12 16:22:48 -0500

    fixed DIRS inside TEMPLATES inside settings.py

--- datavisualizers/settings.py
+++ datavisualizers/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+# SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__)) # temporary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent # original line
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'datavisualizers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'visualizer')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- visualizer/views.py
+++ visualizer/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.template.response import TemplateResponse
 
 # def index(request):
 #     return HttpResponse("This is main function index.")
 
+
+def process_code(code):
+    return 0
 
 
 def visualize(request):
     if request.method == 'POST':
         code = request.POST.get('code')
         visualization_result = process_code(code)
-        return render(request, 'datavisualizer/visualizer/result.html', {'visualization': visualization_result})
-    return render(request, 'datavisualizer/visualizer/visualize.html')
-    # return HttpResponse("HIHI")
+        return render(request, 'result.html', {'visualization': visualization_result})
+    return render(request, 'visualize.html')
+    return HttpResponse("HIHI")
