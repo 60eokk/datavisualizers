d9a80e566b514cfce58bb8c75b2e268540cc9b6c
Keith Jung <60eokk@gmail.com>
2024-06-19 15:50:18 -0500

    print statements to check if ast is working

--- visualizer/animate.py
+++ visualizer/animate.py
@@ -1,7 +1,10 @@
 import ast
 import json
 
+
 def parse_code(code):
+    # print(ast.parse(code))
+    # print("parse_code works")
     return ast.parse(code)
 
 def gen_data(ast_node): # complex code
